66301bdb3799408234e103b372209fc6b3ccd8bc
Tasdik Rahman <git@tasdikrahman.me>
2020-08-15 12:30:52 +0530

    migration: Add migration to add default value of certifcate_expiring column to false in domains table

---
+++ db/migrate/20200815065332_add_default_value_to_certificate_expiring_in_domains.rb
@@ -0,0 +1,5 @@
+class AddDefaultValueToCertificateExpiringInDomains < ActiveRecord::Migration[6.0]
+  def change
+    change_column :domains, :certificate_expiring, :boolean, :default => false
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,14 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_11_154605) do
+ActiveRecord::Schema.define(version: 2020_08_15_065332) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "domains", force: :cascade do |t|
     t.string "fqdn", null: false
-    t.boolean "certificate_expiring"
+    t.boolean "certificate_expiring", default: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
