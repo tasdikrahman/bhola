95081fb600e5c73eeb9a125c9d21e4017ffa8d4e
Tasdik Rahman <git@tasdikrahman.me>
2020-08-16 10:39:48 +0530

    Add asserts on domain model to check for object persistence

--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -5,12 +5,17 @@ RSpec.describe Domain, type: :model do
 
   it 'is not valid without an fqdn present' do
     domain = Domain.new(fqdn: nil)
+
+    expect(domain.save).to be_falsey
     expect(domain).not_to be_valid
+    expect(Domain.all.count).to eq(0)
   end
 
   it 'has certificate_expiring set to false when saving model object' do
     domain = Domain.create(fqdn: 'foo.example.com')
+
     expect(domain.certificate_expiring).to be_falsey
+    expect(Domain.all.count).to eq(1)
   end
 
   it 'has a uniqueness check on the fqdn when trying to create persist object' do
@@ -20,5 +25,6 @@ RSpec.describe Domain, type: :model do
 
     expect(domain2).to_not be_valid
     expect(domain2.errors[:fqdn]).to include("has already been taken")
+    expect(Domain.all.count).to eq(1)
   end
 end
