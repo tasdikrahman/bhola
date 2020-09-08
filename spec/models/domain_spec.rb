3a15db65e7d615e06d520bee519954953d34649d
Tasdik Rahman <git@tasdikrahman.me>
2020-09-08 23:34:41 +0530

    refactor domain model specs to move around contexts

--- spec/models/domain_spec.rb
+++ spec/models/domain_spec.rb
@@ -31,71 +31,73 @@ RSpec.describe Domain, type: :model do
   end
 
   context '#check_certificate' do
-    let(:fqdn) { 'foo.example.com' }
-    let(:domain) { Domain.create(fqdn: fqdn) }
-    let(:port) { 443 }
+    context 'domain inserted is registered' do
+      let(:fqdn) { 'example.com' }
+      let(:domain) { Domain.create(fqdn: fqdn) }
+      let(:port) { 443 }
 
-    context 'connection is successfull' do
-      let(:cert_name) do
-        OpenSSL::X509::Name.new [['CN', 'www.github.com'], ['O', 'Github\, Inc.'], ['L', 'San Francisco'],
-                                 %w[ST California], %w[C US]]
-      end
-      let(:cert_not_before) { Time.parse('2012-10-1 8:00:00 Pacific Time (US & Canada)').utc }
-      let(:cert) { OpenSSL::X509::Certificate.new }
-      let(:threshold_days) { '10' }
-      let(:time_now_stub) { Time.parse('2020-6-2 8:00:00 Pacific Time (US & Canada)').utc }
-
-      before(:each) do
-        allow(Figaro).to receive_message_chain(:env, :certificate_expiry_threshold).and_return(threshold_days)
-        allow(Time).to receive(:now).and_return(time_now_stub)
-        cert.subject = cert_name
-        cert.not_before = cert_not_before
-        cert.not_after = cert_not_after
-        tcpsocket_double = double(TCPSocket)
-        sslcontext_double = double(OpenSSL::SSL::SSLContext)
-        sslsocket_double = double(OpenSSL::SSL::SSLSocket)
-        expect(OpenSSL::SSL::SSLContext).to receive(:new).and_return(sslcontext_double)
-        expect(TCPSocket).to receive(:new).with(domain.fqdn, port).and_return(tcpsocket_double)
-        expect(OpenSSL::SSL::SSLSocket).to receive(:new).with(tcpsocket_double, sslcontext_double).
-          and_return(sslsocket_double)
-        expect(sslsocket_double).to receive(:connect).and_return(sslsocket_double)
-        expect(sslsocket_double).to receive(:peer_cert).and_return(cert)
-      end
-
-      context 'the certificate expiry date is outside of the buffer period set' do
-        let(:cert_not_after) { Time.parse('2030-10-1 8:00:00 Pacific Time (US & Canada)').utc }
-
-        it 'does nothing to the certificate_expiring field' do
-          domain.check_certificate
-
-          expect(Domain.find_by(fqdn: fqdn).certificate_expiring).to be_falsey
+      context 'connection is successfull' do
+        let(:cert_name) do
+          OpenSSL::X509::Name.new [['CN', 'www.github.com'], ['O', 'Github\, Inc.'], ['L', 'San Francisco'],
+                                   %w[ST California], %w[C US]]
         end
-      end
+        let(:cert_not_before) { Time.parse('2012-10-1 8:00:00 Pacific Time (US & Canada)').utc }
+        let(:cert) { OpenSSL::X509::Certificate.new }
+        let(:threshold_days) { '10' }
+        let(:time_now_stub) { Time.parse('2020-6-2 8:00:00 Pacific Time (US & Canada)').utc }
 
-      context 'the certificate is about to expire within the buffer period set' do
-        let(:cert_not_after) { Time.parse('2020-6-10 8:00:00 Pacific Time (US & Canada)').utc }
+        before(:each) do
+          allow(Figaro).to receive_message_chain(:env, :certificate_expiry_threshold).and_return(threshold_days)
+          allow(Time).to receive(:now).and_return(time_now_stub)
+          cert.subject = cert_name
+          cert.not_before = cert_not_before
+          cert.not_after = cert_not_after
+          tcpsocket_double = double(TCPSocket)
+          sslcontext_double = double(OpenSSL::SSL::SSLContext)
+          sslsocket_double = double(OpenSSL::SSL::SSLSocket)
+          expect(OpenSSL::SSL::SSLContext).to receive(:new).and_return(sslcontext_double)
+          expect(TCPSocket).to receive(:new).with(domain.fqdn, port).and_return(tcpsocket_double)
+          expect(OpenSSL::SSL::SSLSocket).to receive(:new).with(tcpsocket_double, sslcontext_double).
+            and_return(sslsocket_double)
+          expect(sslsocket_double).to receive(:connect).and_return(sslsocket_double)
+          expect(sslsocket_double).to receive(:peer_cert).and_return(cert)
+        end
 
-        it 'updates the certificate_expiring field to be true' do
-          domain.check_certificate
+        context 'the certificate expiry date is outside of the buffer period set' do
+          let(:cert_not_after) { Time.parse('2030-10-1 8:00:00 Pacific Time (US & Canada)').utc }
 
-          expect(Domain.find_by(fqdn: fqdn).certificate_expiring).to be_truthy
+          it 'does nothing to the certificate_expiring field' do
+            domain.check_certificate
+
+            expect(Domain.find_by(fqdn: fqdn).certificate_expiring).to be_falsey
+          end
+        end
+
+        context 'the certificate is about to expire within the buffer period set' do
+          let(:cert_not_after) { Time.parse('2020-6-10 8:00:00 Pacific Time (US & Canada)').utc }
+
+          it 'updates the certificate_expiring field to be true' do
+            domain.check_certificate
+
+            expect(Domain.find_by(fqdn: fqdn).certificate_expiring).to be_truthy
+          end
         end
       end
     end
 
-    context 'connection is unsuccessful' do
-      context 'when domain inserted is not a valid domain' do
-        let(:fqdn) { 'invalid-domain.com' }
-        let(:error_message) { 'getaddrinfo: Name or service not known' }
+    context 'domain inserted is not registered' do
+      let(:port) { 443 }
+      let(:fqdn) { 'invalid-domain.com' }
+      let(:domain) { Domain.create(fqdn: fqdn) }
+      let(:error_message) { 'getaddrinfo: Name or service not known' }
 
-        it 'logs a SocketError with getaddrinfo' do
-          allow(TCPSocket).to receive(:new).with(fqdn, port).and_raise(SocketError, error_message)
-          allow(Rails.logger).to receive(:error)
+      it 'logs a SocketError with getaddrinfo' do
+        allow(TCPSocket).to receive(:new).with(fqdn, port).and_raise(SocketError, error_message)
+        allow(Rails.logger).to receive(:error)
 
-          domain.check_certificate
+        domain.check_certificate
 
-          expect(Rails.logger).to have_received(:error).with("Error connecting to #{fqdn}, error: #{error_message}")
-        end
+        expect(Rails.logger).to have_received(:error).with("Error connecting to #{fqdn}, error: #{error_message}")
       end
     end
   end
