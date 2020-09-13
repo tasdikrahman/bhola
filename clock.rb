7cc428b3de0d1289ac3d3690480802cc9d61daac
Tasdik Rahman <git@tasdikrahman.me>
2020-09-13 21:33:19 +0530

    chore: set check_certificate_job_retrigger_threshold to 10 seconds on dev env

--- clock.rb
+++ clock.rb
@@ -5,5 +5,7 @@ require './config/environment'
 
 # Clockwork module triggers CheckCertificateJob.method to trigger certificate_expiring? on all the stored domains
 module Clockwork
-  every(Figaro.env.check_certificate_job_retrigger_threshold.to_i.second, 'check_certificate.perform') { CheckCertificateJob.perform_now }
+  every(Figaro.env.check_certificate_job_retrigger_threshold.to_i.second, 'check_certificate_job.perform_now') do
+    CheckCertificateJob.perform_now
+  end
 end
