487cd4a9690e0449d9f32d954fca9a78e5abe931
Tasdik Rahman <git@tasdikrahman.me>
2020-09-13 11:22:03 +0530

    feat: Add spec for CheckCertificateJob#perform for case when there are no domains being tracked

--- app/jobs/check_certificate_job.rb
+++ app/jobs/check_certificate_job.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
+# CheckCertificateJob will iterate over all the Domain objects stored and call the method #check_certificate on it
 class CheckCertificateJob < ApplicationJob
+  def perform(*)
+    Rails.logger.info('No domains are tracked as of now, please insert domains to be tracked') if Domain.all.count.zero?
+  end
 end
--- spec/jobs/check_certificate_job_spec.rb
+++ spec/jobs/check_certificate_job_spec.rb
@@ -1,5 +1,21 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe CheckCertificateJob, type: :job do
   it { expect(described_class).to be < ApplicationJob }
+
+  context '#perform' do
+    context 'when there are no domains being tracked' do
+      it 'will log via rails logger that there are no domains being tracked' do
+        allow(Rails.logger).to receive(:info)
+        allow(Domain).to receive_message_chain(:all, :count).and_return(0)
+
+        CheckCertificateJob.perform_now
+
+        expect(Rails.logger).to have_received(:info).
+          with('No domains are tracked as of now, please insert domains to be tracked')
+      end
+    end
+  end
 end
