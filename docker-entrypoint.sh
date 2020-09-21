9e293fa23fe2770fff4a1791bfa2694c5366877f
Tasdik Rahman <git@tasdikrahman.me>
2020-09-21 23:30:39 +0530

    chore: remove js packs which were causing compile step to fail

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -5,8 +5,4 @@ if [ -f tmp/pids/server.pid ]; then
   rm tmp/pids/server.pid
 fi
 
-bundle check || bundle install --binstubs="$BUNDLE_BIN"
-
-bundle exec rake assets:precompile --trace
-
 exec bundle exec "$@"
