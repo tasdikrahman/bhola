ba298a985563c013f04a2ef9f4dfa5e43a67fad6
Tasdik Rahman <git@tasdikrahman.me>
2020-09-21 21:59:23 +0530

    chore: remove require jquery3 from application js require to try fix precompile step

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,8 +8,8 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
-require("jquery3")
-require("popper")
+//require("jquery3")
+//require("popper")
 require("bootstrap-sprockets")
 
 // Uncomment to copy all static images under ../images to the output folder and reference
